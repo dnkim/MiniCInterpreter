194dd028211b7d78cd3256b3fd60ad9899ae5f77
Dongnak Kim <friend6044@kaist.ac.kr>
2019-11-30 21:18:00 +0900

    Update Interpreter.py skeleton

--- Interpreter.py
+++ Interpreter.py
@@ -1,6 +1,33 @@
 from Frame import Frame
 from Command import Command
 
+MAIN = "main"
+
 class Interpreter:
 	def __init__(self):
-		#unimplemented
+		self.frame = Frame()
+		self.command = Command()
+
+		# build AST
+
+		self.handle_goal(goal)
+
+	def handle_goal(self, goal):
+		for func in goal.funcs:
+			self.handle_func(func)
+
+		main = self.frame.get_function(MAIN)
+
+		# execute main
+
+	def handle_func(self, func):
+		self.frame.add_function(func.name, func)
+
+	def handle_factor2(self, factor2);
+		func = self.frame.get_function(factor2.func_name)
+
+		frame.into_function()
+
+		# execute func
+
+		frame.escape_scope()

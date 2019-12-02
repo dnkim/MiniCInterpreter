104424de5d2859249f4cf6d06e5608ba0b730bbd
emutek <57401502+emutek@users.noreply.github.com>
2019-12-02 17:27:02 +0900

    Reflected changes to grammar in Parsing suite
    For ease of implementing "next"
    Makefile.bat works if you have a "results" folder inside the "testfiles" folder

---
+++ Makefile.bat
@@ -0,0 +1,18 @@
+set CC=python
+set TESTER=LexParTester.py
+set TEST_OPT=par
+set TEST_DIR=testfiles
+
+echo %TEST_DIR%
+
+if %1 == test (
+for /f %%f in ('dir /b .\%TEST_DIR%\*.txt') do %CC% %TESTER% %TEST_OPT% .\%TEST_DIR%\%%f .\%TEST_DIR%\results\%%f
+)
+
+if %1 ==clean (
+	del .\%TEST_DIR%\results\*.txt
+)
+
+if %1 == test2 (
+for /f %%f in ('dir /b .\%TEST_DIR%\*.txt') do echo %%f
+)

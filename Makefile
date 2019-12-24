980b03e18d2f3efe3cca007778838d678ea131ec
Dongnak Kim <friend6044@kaist.ac.kr>
2019-12-24 20:58:15 +0900

    Update interpreter options

--- Makefile
+++ Makefile
@@ -1,12 +1,19 @@
 CC = python3
 TESTER = src/MiniCInterpreter.py
-TEST_OPT = test
+TEST_OPT = -t
+RTEST_OPT = -t -r
 TEST_DIR = testfiles
 
 test:
 	@mkdir -p $(TEST_DIR)/results
 	@for f in $(notdir $(wildcard $(TEST_DIR)/*.txt)); do \
-		$(CC) $(TESTER) $(TEST_DIR)/$$f $(TEST_OPT) > $(TEST_DIR)/results/$$f; \
+		$(CC) $(TESTER) $(TEST_OPT) $(TEST_DIR)/$$f > $(TEST_DIR)/results/$$f; \
+	done
+
+rtest:
+	@mkdir -p $(TEST_DIR)/results
+	@for f in $(notdir $(wildcard $(TEST_DIR)/*.txt)); do \
+		$(CC) $(TESTER) $(RTEST_OPT) $(TEST_DIR)/$$f > $(TEST_DIR)/results/$$f; \
 	done
 
 clean:
--- src/MiniCInterpreter.py
+++ src/MiniCInterpreter.py
@@ -5,10 +5,19 @@ from InterpreterRecc import InterpreterRecc
 import sys
 
 def main():
-	if not (len(sys.argv) == 2 or (len(sys.argv) == 3 and sys.argv[2] == "test") or (len(sys.argv)==4 and sys.argv[3] == 'recc')):
+	if len(sys.argv) < 2:
 		print("Incorrect command usage")
 		return
-	file_name = sys.argv[1]
+	test, recc = False, False
+	for opt in sys.argv[1:-1]:
+		if opt == "-t" and not test:
+			test = True
+		elif opt == "-r" and not recc:
+			recc = True
+		else:
+			print("Incorrect command usage")
+			return
+	file_name = sys.argv[len(sys.argv) - 1]
 	with open(file_name) as file_inp:
 		lex = Lex(file_inp, True)
 		par = Par(lex)
@@ -17,9 +26,9 @@ def main():
 			return
 		file_inp.seek(0)
 		actual_code_lines = [line.rstrip() for line in file_inp]
-		no_command = True if (len(sys.argv) >= 3 and sys.argv[2] == "test") else False
-		Interpreter_To_Use = InterpreterRecc if (len(sys.argv) >= 4 and sys.argv[3] == 'recc') else Interpreter
-		interp = Interpreter_To_Use(goal, True, actual_code_lines, no_command)
+		no_command = True if test else False
+		Interpreter_To_Use = InterpreterRecc if recc else Interpreter
+		interp = Interpreter_To_Use(goal, False, actual_code_lines, no_command)
 
 if __name__ == "__main__":
 	main()
